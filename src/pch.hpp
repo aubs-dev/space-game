0f8a6f230ab76f0ef76a0f470c1c4655ccae9876
Aubrey Dominik <hello@aubs.dev>
2025-08-05 04:39:46 +0100

    build: added base pch file

---
+++ src/pch.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+// -------------------------------------------
+// System
+// -------------------------------------------
+
+#include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <windowsx.h>
+
+#include <type_traits>
+
+// -------------------------------------------
+// Vendor
+// -------------------------------------------
+
+#include "vendor/stb_image.h"
+#include "vendor/stb_image_write.h"
+#include "vendor/stb_rect_pack.h"
+
+// TODO: Implement my own version of this in 'filesystem'
+#include "vendor/dirent.h"
+
+#include "vendor/glad/gl.h"
+#include "vendor/glad/wgl.h"
